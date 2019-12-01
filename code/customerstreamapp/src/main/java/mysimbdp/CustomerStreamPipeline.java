9bf957be68f69c284e880304eb664e0ca194f6d2
sourserge <15277406+souserge@users.noreply.github.com>
2019-12-01 18:17:04 +0200

    Add data producing

--- code/customerstreamapp/src/main/java/mysimbdp/CustomerStreamPipeline.java
+++ code/customerstreamapp/src/main/java/mysimbdp/CustomerStreamPipeline.java
@@ -23,7 +23,6 @@ import org.apache.beam.sdk.values.PCollection;
 import org.apache.beam.sdk.io.rabbitmq.RabbitMqIO;
 import org.apache.beam.sdk.io.rabbitmq.RabbitMqMessage;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
-import org.apache.beam.sdk.transforms.Create;
 import org.apache.beam.sdk.transforms.DoFn;
 import org.apache.beam.sdk.transforms.MapElements;
 import org.apache.beam.sdk.transforms.ParDo;
@@ -33,62 +32,62 @@ import org.slf4j.LoggerFactory;
 
 /**
  *
- * <p>To run the pipeline using managed resource in Google Cloud
- * Platform, you should specify the following command-line options:
- *   --project=<YOUR_PROJECT_ID>
- *   --stagingLocation=<STAGING_LOCATION_IN_CLOUD_STORAGE>
- *   --runner=DataflowRunner
+ * <p>
+ * To run the pipeline using managed resource in Google Cloud Platform, you
+ * should specify the following command-line options:
+ * --project=<YOUR_PROJECT_ID>
+ * --stagingLocation=<STAGING_LOCATION_IN_CLOUD_STORAGE> --runner=DataflowRunner
  */
 public class CustomerStreamPipeline {
   private static final Logger LOG = LoggerFactory.getLogger(CustomerStreamPipeline.class);
 
   public static void main(String[] args) {
-    this.waitForRabbitMQ(10);
-
-    Pipeline p = Pipeline.create(
-        PipelineOptionsFactory.fromArgs(args).withValidation().create());
-
-    String uri = this.getConnectionUri();
-    String queue = this.getQueueName();
-
-    PCollection<RabbitMqMessage> messages = p.apply(
-        RabbitMqIO.read().withUri(uri).withQueue(queue));
-    
-    messages
-    .apply(MapElements.via(new SimpleFunction<RabbitMqMessage, String>() {
-      @Override
-      public String apply(RabbitMqMessage input) {
-        return new String(input.getBody());
-      }
-    }))
-    .apply(ParDo.of(new DoFn<String, Void>() {
+    DoFn<String[], Void> logMessages = new DoFn<String[], Void>() {
       @ProcessElement
-      public void processElement(ProcessContext c)  {
-        LOG.info(c.element());
+      public void processElement(ProcessContext c) {
+        String msg = String.join(",", c.element());
+        LOG.info(msg);
       }
-    }));
+    };
+
+    SimpleFunction<RabbitMqMessage, String[]> parseMessages = new SimpleFunction<RabbitMqMessage, String[]>() {
+      @Override
+      public String[] apply(RabbitMqMessage input) {
+        String body = new String(input.getBody());
+        String[] row = body.split("\n");
+        return row;
+      }
+    };
+
+    CustomerStreamPipeline.waitForRabbitMQ(10);
+
+    Pipeline p = Pipeline.create(PipelineOptionsFactory.fromArgs(args).withValidation().create());
+
+    String uri = CustomerStreamPipeline.getConnectionUri();
+    String queue = CustomerStreamPipeline.getQueueName();
+
+    PCollection<RabbitMqMessage> messages = p.apply(RabbitMqIO.read().withUri(uri).withQueue(queue));
+
+    messages.apply(MapElements.via(parseMessages)).apply(ParDo.of(logMessages));
 
     p.run();
   }
 
-  private String getConnectionUri() {
+  private static String getConnectionUri() {
     String user = System.getenv("RABBITMQ_DEFAULT_USER");
     String pass = System.getenv("RABBITMQ_DEFAULT_PASS");
     String host = System.getenv("RABBITMQ_HOST");
     String port = "5672";
-    String[] params = {
-      "retry_delay=5",
-      "connection_attempts=5"
-    };
+    String[] params = { "retry_delay=5", "connection_attempts=5" };
 
-    return "amqp://" + user + ":" + pass + "@" + host + '?' + String.join("&", params);
+    return "amqp://" + user + ":" + pass + "@" + host + ":" + port + '?' + String.join("&", params);
   }
 
-  private String getQueueName() {
+  private static String getQueueName() {
     return "customerstreamapp";
   }
 
-  private void waitForRabbitMQ(int seconds) {
+  private static void waitForRabbitMQ(int seconds) {
     try {
       TimeUnit.SECONDS.sleep(seconds);
     } catch (InterruptedException e) {
--- code/sensorapp/src/main.py
+++ code/sensorapp/src/main.py
@@ -1,5 +1,6 @@
 # Builtin imports
 import os
+import csv
 
 # 3rd party imports
 import pika
@@ -8,8 +9,27 @@ RABBITMQ_HOST = os.environ['RABBITMQ_HOST']
 RABBITMQ_USERNAME = os.environ['RABBITMQ_DEFAULT_USER']
 RABBITMQ_PASSWORD = os.environ['RABBITMQ_DEFAULT_PASS']
 
+QUEUE_NAME = 'customerstreamapp'
+DATA_PATH = '/app/data/data.csv'
+EXCHANGE_DEFAULT = ''
+
 
 def main():
+    def send_data(channel):
+        channel.queue_declare(queue=QUEUE_NAME)
+
+        def publish_to_rabbit(row):
+            body = '\n'.join(row)
+
+            channel.basic_publish(
+                exchange=EXCHANGE_DEFAULT, routing_key=QUEUE_NAME, body=body)
+
+        process_data_stream(publish_to_rabbit)
+
+    use_rabbit_channel(send_data)
+
+
+def use_rabbit_channel(callback):
     credentials = pika.credentials.PlainCredentials(
         username=RABBITMQ_USERNAME, password=RABBITMQ_PASSWORD)
 
@@ -22,14 +42,19 @@ def main():
     connection = pika.BlockingConnection(connection_parameters)
     channel = connection.channel()
 
-    channel.queue_declare(queue='hello')
+    callback(channel)
 
-    channel.basic_publish(
-        exchange='', routing_key='hello', body='Hello World!')
-    print(" [x] Sent 'Hello World!'")
     connection.close()
 
 
+def process_data_stream(processor):
+    with open(DATA_PATH) as csvfile:
+        data_stream = csv.reader(csvfile, strict=True)
+        header = next(data_stream, None)
+        for row in data_stream:
+            processor(row)
+
+
 if __name__ == '__main__':
-    print('Starting producer')
+    print('Starting sensor app')
     main()
